892e078aa8358ebc87a91fd198dea37a67831654
kim-na-ram <snrneh3@naver.com>
2024-09-12 17:23:06 +0900

    fix(comment): 잘못된 logging annotation 위치 수정

--- src/main/java/org/example/expert/domain/comment/controller/CommentAdminController.java
+++ src/main/java/org/example/expert/domain/comment/controller/CommentAdminController.java
@@ -1,11 +1,13 @@
 package org.example.expert.domain.comment.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.annotation.AdminLogger;
 import org.example.expert.domain.comment.service.CommentAdminService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@AdminLogger
 @RestController
 @RequiredArgsConstructor
 public class CommentAdminController {
--- src/main/java/org/example/expert/domain/comment/service/CommentAdminService.java
+++ src/main/java/org/example/expert/domain/comment/service/CommentAdminService.java
@@ -1,13 +1,11 @@
 package org.example.expert.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.expert.annotation.AdminLogger;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@AdminLogger
 @RequiredArgsConstructor
 public class CommentAdminService {
 
